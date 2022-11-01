9b1dbb4ee53d93c1b414a933e12708404b3972ed
FuexFollets <fuex9follets@gmail.com>
2022-11-01 18:22:10 -0400

    add: file syncing

---
+++ sync.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+
+included=$(cat included)
+
+search_phrase=$(cat included | tr "\n" "|" | rev | cut -c2- | rev)
+
+search_phrase="($search_phrase)"
+
+included_directories=$(ls -d ~/.config/* | egrep $search_phrase)
+
+cp -r $included_directories .
