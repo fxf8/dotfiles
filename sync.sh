9f11052a949b4481044cb61a1eeb945c5995424d
FuexFollets <fuex9follets@gmail.com>
2022-12-20 02:27:03 -0500

    Edited sync conditions

--- sync.sh
+++ sync.sh
@@ -6,6 +6,6 @@ search_phrase=$(cat included | tr "\n" "|" | rev | cut -c2- | rev)
 
 search_phrase="($search_phrase)"
 
-included_directories=$(ls -d ~/.config/* | egrep $search_phrase)
+included_directories=$(ls -d ~/user-configs/* | egrep $search_phrase)
 
 cp -r $included_directories .
