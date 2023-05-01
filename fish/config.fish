0fc20fcd580cabd6c9b24277e4c98068045ddee0
FuexFollets <fuex9follets@gmail.com>
2023-05-01 10:55:29 -0400

    Updated fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,23 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    source ~/.fishrc
+
+    cd
+    alias nv "nvim"
+    alias lsa "ls -AChs --group-directories-first"
+
+    export PATH="$PATH:/home/fuexfollets/.cargo/bin" # cargo binaries
+    export PATH="$PATH:/usr/lib/jvm/java-19-openjdk/bin" # openjdk-19 binaries
+    export PATH="$PATH:/home/fuexfollets/go/bin" # golang binaries
+
+    # fish_vi_key_bindings
+
+    # function fish_user_key_bindings
+    #    bind \cf forward-word
+    #    bind \ef forward-char
+    # end
+
+    export EDITOR="nvim"
+    neofetch
 end
 
 set PREV_COMMANDS_RUN 0
