6d10247610ef3c5f10b368b1eb31b8d94cd7b25c
FuexFollets <fuex9follets@gmail.com>
2023-09-25 13:16:08 -0400

    Added java binaries to path and removed automatic cd

--- fish/config.fish
+++ fish/config.fish
@@ -1,25 +1,19 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-    cd
     alias nv "nvim"
     alias lsa "ls -AChs --group-directories-first"
 
     export PATH="$PATH:$HOME/.cargo/bin" # cargo binaries
-    export PATH="$PATH:/usr/lib/jvm/java-19-openjdk/bin" # openjdk-19 binaries
     export PATH="$PATH:$HOME/go/bin" # golang binaries
     export PATH="$PATH:/usr/lib/emscripten"
     export PATH="$PATH:$HOME/.local/bin"
+    export PATH="$PATH:/usr/lib/jvm/java-20-openjdk/bin/"
 
-    # fish_vi_key_bindings
-
-    # function fish_user_key_bindings
-    #    bind \cf forward-word
-    #    bind \ef forward-char
-    # end
+    fish_vi_key_bindings
+    bind --mode insert \cf accept-autosuggestion
 
     export EDITOR="nvim"
-    neofetch
 end
 
 set PREV_COMMANDS_RUN 0
@@ -31,3 +25,5 @@ alias clipboard="xclip -sel clip"
 alias icat="kitty +kitten icat"
 
 export PATH="/usr/local/shortcuts:$PATH"
+
+thefuck --alias | source
