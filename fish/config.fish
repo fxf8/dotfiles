246be77d4b0824a1eac7f07a268d49cd563a285f
FuexFollets <fuex9follets@gmail.com>
2023-10-15 16:39:20 -0400

    Added extra paths and replaced env syntax

--- fish/config.fish
+++ fish/config.fish
@@ -4,11 +4,21 @@ if status is-interactive
     alias nv "nvim"
     alias lsa "ls -AChs --group-directories-first"
 
-    export PATH="$PATH:$HOME/.cargo/bin" # cargo binaries
-    export PATH="$PATH:$HOME/go/bin" # golang binaries
-    export PATH="$PATH:/usr/lib/emscripten"
-    export PATH="$PATH:$HOME/.local/bin"
-    export PATH="$PATH:/usr/lib/jvm/java-20-openjdk/bin/"
+    set -x PATH "$PATH:$HOME/.cargo/bin" # cargo binaries
+    set -x PATH "$PATH:$HOME/go/bin" # golang binaries
+    set -x PATH "$PATH:/usr/lib/emscripten"
+    set -x PATH "$PATH:$HOME/.local/bin"
+    set -x PATH "$PATH:/usr/lib/jvm/java-21-openjdk/bin/"
+
+    set -x ANDROID_HOME "/opt/android-sdk"
+    set -x PATH "$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin"
+
+    set -x JAVA_HOME "/usr/lib/jvm/java-8-openjdk/"
+    set -x ANDROID_SDK_ROOT "/opt/android-sdk"
+
+    set -x QT_QPA_PLATFORM_PLUGIN_PATH "/opt/android-sdk/emulator/lib64/qt/"
+
+
 
     fish_vi_key_bindings
     bind --mode insert \cf accept-autosuggestion
