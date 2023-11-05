2ab237d99932fbe39508694256842efbe7870fa2
FuexFollets <fuex9follets@gmail.com>
2023-11-05 17:50:39 -0500

    Added configuration for undotree and persist mode

--- nvim/after/plugin/undotree.lua
+++ nvim/after/plugin/undotree.lua
@@ -1,4 +1,18 @@
---[[ Doesnt work for some reason
-vim.keymap.set('n', "<leader>ut", vim.cmd("UndotreeToggle"))
-vim.cmd("normal  <leader>ut")
---]]
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+
+-- vimscript
+
+vim.cmd [[
+if has("persistent_undo")
+   let target_path = expand('~/.undodir')
+
+    " create the directory and any parent directories
+    " if the location does not exist.
+    if !isdirectory(target_path)
+        call mkdir(target_path, "p", 0700)
+    endif
+
+    let &undodir=target_path
+    set undofile
+endif
+]]
