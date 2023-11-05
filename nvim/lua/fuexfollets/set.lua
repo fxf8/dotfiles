8c667f50da02ccb56007676aa1e73e7297f2e163
FuexFollets <fuex9follets@gmail.com>
2023-11-05 18:09:58 -0500

    Added additional keybinds for writing, quiting, and spliting the window

--- nvim/lua/fuexfollets/set.lua
+++ nvim/lua/fuexfollets/set.lua
@@ -18,6 +18,22 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "<leader>w", vim.cmd.w) -- Write to current file
 vim.keymap.set("n", "<leader>W", vim.cmd.wall) -- Write to all
-
 vim.keymap.set("n", "<leader>q", vim.cmd.q) -- Quit
 vim.keymap.set("n", "<leader>Q", function() vim.cmd("q!") end) -- Quit without saving
+vim.keymap.set("n", "<leader>e", vim.cmd.qall) -- Quit all 'E'
+vim.keymap.set("n", "<leader>E", function() vim.cmd("qa!") end) -- Quit all without saving
+-- Write and quit to all 'A"
+vim.keymap.set("n", "<leader>A", vim.cmd.wqa)
+
+-- Split based on current window size
+vim.keymap.set("n", "<leader>s", function()
+    local width = vim.api.nvim_win_get_width(0)
+    local height = vim.api.nvim_win_get_height(0)
+    local char_ratio = 135 / 55 -- window width / height
+
+    if width / height > char_ratio then
+        vim.cmd.vsplit()
+    else
+        vim.cmd.split()
+    end
+end) -- Split
