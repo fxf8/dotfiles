52d7fd0982326ee360fc4978440a707374d84577
FuexFollets <fuex9follets@gmail.com>
2023-11-05 17:48:00 -0500

    Added configuration for write and quit keybindings

--- nvim/lua/fuexfollets/set.lua
+++ nvim/lua/fuexfollets/set.lua
@@ -15,3 +15,9 @@ vim.opt.wrap = true
 vim.opt.swapfile = false
 
 vim.g.mapleader = " "
+
+vim.keymap.set("n", "<leader>w", vim.cmd.w) -- Write to current file
+vim.keymap.set("n", "<leader>W", vim.cmd.wall) -- Write to all
+
+vim.keymap.set("n", "<leader>q", vim.cmd.q) -- Quit
+vim.keymap.set("n", "<leader>Q", function() vim.cmd("q!") end) -- Quit without saving
