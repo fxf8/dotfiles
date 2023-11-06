ad7d4bf2baf1be2ed78f39f2d23d5b26bd5de79c
FuexFollets <fuex9follets@gmail.com>
2023-11-06 14:44:51 -0500

    Added keybind for git fugitive window

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,2 +1,3 @@
+vim.keymap.set("n", "<leader>go", vim.cmd.Git)
 vim.keymap.set("n", "<leader>do", function() vim.cmd("Gdiffsplit") end, {})
 vim.keymap.set("v", "<leader>dp", function() vim.cmd("diffput") end, {})
