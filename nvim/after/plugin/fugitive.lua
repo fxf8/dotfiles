31c47f1ff0b4981044fb5ed6854f2ec7a80b77f4
FuexFollets <fuex9follets@gmail.com>
2023-11-06 20:19:34 -0500

    Added visual selection for diffput keybind `dp`

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,3 +1,10 @@
 vim.keymap.set("n", "<leader>go", vim.cmd.Git)
 vim.keymap.set("n", "<leader>do", function() vim.cmd("Gdiffsplit") end, {})
-vim.keymap.set("v", "<leader>dp", function() vim.cmd("diffput") end, {})
+
+local function diffput_visual_selection()
+    local start_line, end_line = vim.fn.line("'<"), vim.fn.line("'>")
+
+    vim.cmd(string.format("%d,%ddiffput", start_line, end_line))
+end
+
+vim.keymap.set("v", "<leader>dp", diffput_visual_selection, {})
