8c03f1ed0d37a997d3767c8035cbb2a7266f2ceb
FuexFollets <fuex9follets@gmail.com>
2023-11-07 08:41:33 -0500

    Added range fixing for diffput

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -3,8 +3,10 @@ vim.keymap.set("n", "<leader>do", function() vim.cmd("Gdiffsplit") end, {})
 
 local function diffput_visual_selection()
     local start_line, end_line = vim.fn.line("'<"), vim.fn.line("'>")
+    local min = math.min(start_line, end_line)
+    local max = math.max(start_line, end_line)
 
-    vim.cmd(string.format("%d,%ddiffput", start_line, end_line))
+    vim.cmd(string.format("%d,%ddiffput", min, max))
 end
 
 vim.keymap.set("v", "<leader>dp", diffput_visual_selection, {})
