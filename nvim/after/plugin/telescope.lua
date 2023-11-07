a83b4b04472494180131a0b5b40b11746987f37e
FuexFollets <fuex9follets@gmail.com>
2023-11-07 08:41:59 -0500

    Fixed issue with telescope folds

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,3 +4,4 @@ vim.keymap.set('n', "<leader>pf", builtin.find_files, {}) -- For general files
 vim.keymap.set('n', "<leader>pg", builtin.git_files, {}) -- For current git directory
 vim.keymap.set('n', "<leader>ps", builtin.live_grep, {}) -- Grep file search
 
+vim.api.nvim_create_autocmd({ "BufEnter" }, { pattern = { "*" }, command = "normal zx", })
