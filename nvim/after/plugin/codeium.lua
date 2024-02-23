b809dbe4fb95d28a310a2a3d2ee609cf93b3fd8d
FuexFollets <fuex9follets@gmail.com>
2024-02-23 14:49:07 -0500

    Fixed copilot and codeium configurations

--- nvim/after/plugin/codeium.lua
+++ nvim/after/plugin/codeium.lua
@@ -1,3 +1,15 @@
+--[[
+ ________  ________  ________  _______   ___  ___  ___  _____ ______
+|\   ____\|\   __  \|\   ___ \|\  ___ \ |\  \|\  \|\  \|\   _ \  _   \
+\ \  \___|\ \  \|\  \ \  \_|\ \ \   __/|\ \  \ \  \\\  \ \  \\\__\ \  \
+ \ \  \    \ \  \\\  \ \  \ \\ \ \  \_|/_\ \  \ \  \\\  \ \  \\|__| \  \
+  \ \  \____\ \  \\\  \ \  \_\\ \ \  \_|\ \ \  \ \  \\\  \ \  \    \ \  \
+   \ \_______\ \_______\ \_______\ \_______\ \__\ \_______\ \__\    \ \__\
+    \|_______|\|_______|\|_______|\|_______|\|__|\|_______|\|__|     \|__|
+]]
+   --
+
+--[[
 local accept = function() return vim.fn['codeium#Accept']() end
 local cycle1 = function() return vim.fn['codeium#CycleCompletions'](1) end
 local cycle2 = function() return vim.fn['codeium#CycleCompletions'](-1) end
@@ -9,3 +21,4 @@ vim.keymap.set('i', '<C-J>', accept, opts)
 vim.keymap.set('i', '<C-H>', cycle1, opts)
 vim.keymap.set('i', '<C-K>', cycle2, opts)
 vim.keymap.set('i', '<C-X>', clear, opts)
+]]--
