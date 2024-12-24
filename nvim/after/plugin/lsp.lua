b5adc1e742290f1ccf8b8c8d9a26669db844b263
Fuexfollets <fuex9follets@gmail.com>
2024-12-23 21:14:09 -0500

    Fixed lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,11 +1,16 @@
-local lsp = require('lsp-zero').preset({})
+local lsp = require('lsp-zero')
 local lspconfig = require('lspconfig')
 local neodev = require('neodev')
+local cmp = require('cmp')
+
 local inlay_hints = require('lsp-inlayhints')
+inlay_hints.setup()
 
 neodev.setup({})
 
 -- lsp.preset('recommended') -- Default presets
+
+--[[
 lsp.set_preferences({
     call_servers = 'local',
     cmp_capabilities = true,
@@ -21,29 +26,39 @@ lsp.set_preferences({
         info = ''
     }
 })
+]]
 
-lsp.on_attach(function(client, bufnr)
-    local options = { buffer = bufnr, remap = false }
+vim.opt.signcolumn = 'yes'
 
-    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, options)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, options)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, options)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, options)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, options)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, options)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, options)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, options)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, options)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, options)
-    vim.keymap.set("n", "<leader>lzf", function() vim.lsp.buf.format() end, options)
-    vim.keymap.set("v", "<leader>lzf", function() vim.lsp.buf.format() end, options)
-end)
+local lspconfig_defaults = require('lspconfig').util.default_config
 
-inlay_hints.setup()
+lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+    'force',
+    lspconfig_defaults.capabilities,
+    require('cmp_nvim_lsp').default_capabilities()
+)
 
-lsp.configure('cmake', {
-    root_dir = lspconfig.util.root_pattern('CMakeLists.txt')
+vim.api.nvim_create_autocmd('LspAttach', {
+    desc = 'LSP actions',
+    callback = function(event)
+        local options = { buffer = event.buf }
+
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, options)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, options)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, options)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, options)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, options)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, options)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, options)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, options)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, options)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, options)
+        vim.keymap.set("n", "<leader>lzf", function() vim.lsp.buf.format() end, options)
+        vim.keymap.set("v", "<leader>lzf", function() vim.lsp.buf.format() end, options)
+    end,
 })
+
+require('lspconfig').lua_ls.setup({})
 
 lspconfig.clangd.setup({
     cmd = { 'clangd', '--background-index', '--clang-tidy',
@@ -95,4 +110,17 @@ vim.diagnostic.config({
     severity_sort = true,
     float = true,
     virtual_lines = false -- { only_current_line = true }
+})
+
+cmp.setup({
+    sources = {
+        { name = 'nvim_lsp' },
+    },
+    snippet = {
+        expand = function(args)
+            -- You need Neovim v0.10 to use vim.snippet
+            vim.snippet.expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({}),
 })
