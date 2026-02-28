e4c628b7f57dd83d178038cdab7190ba432fbd7a
fxf8 <fuex9follets@gmail.com>
2026-02-28 15:40:55 -0500

    Added "-" keybind as substitute for "`"

--- nvim/lua/fuexfollets/set.lua
+++ nvim/lua/fuexfollets/set.lua
@@ -103,3 +103,6 @@ vim.keymap.set("n", "j", "gj")
 vim.keymap.set("n", "k", "gk")
 vim.keymap.set("v", "j", "gj")
 vim.keymap.set("v", "k", "gk")
+
+vim.keymap.set("n", "-", "`")
+vim.keymap.set("v", "-", "`")
